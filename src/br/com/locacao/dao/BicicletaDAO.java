a7063c7fce51509be31243250527d1210f02f141
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-07-10 14:15:53 -0300

    método de cadastrar bicicleta adicionado

--- src/br/com/locacao/dao/BicicletaDAO.java
+++ src/br/com/locacao/dao/BicicletaDAO.java
@@ -2,35 +2,25 @@ package br.com.locacao.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import br.com.locacao.modelo.Bicicleta;
 
 public class BicicletaDAO {
-	
-	private Connection connection;
-	
-	public BicicletaDAO(Connection connection){
-		this.connection = connection;
-	}
-	
-	public List<Bicicleta> listarBicicletas() throws SQLException {
-		List<Bicicleta> listaBicicletas = new ArrayList<Bicicleta>();
-		String sql = "SELECT ID, ARO, COR, MARCHAS FROM BICICLETA";
-		try(PreparedStatement pstm = connection.prepareStatement(sql)){
-			pstm.execute();
-			try(ResultSet rst = pstm.getResultSet()){
-				while(rst.next()) {
-					Bicicleta bicicleta = 
-							new Bicicleta(rst.getInt(1), rst.getInt(2), rst.getString(3), rst.getInt(4), null);
-					listaBicicletas.add(bicicleta);
-				}
+
+	public void adicionaBicicleta(Bicicleta bicicleta) throws SQLException {
+		String sql = "INSERT INTO bicicleta(ARO, COR, MARCHAS) VALUES(?,?,?)";
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.setInt(1, bicicleta.getAro());
+				pstm.setString(2, bicicleta.getCor());
+				pstm.setInt(3, bicicleta.getMarchas());
+				pstm.execute();
 			}
-			return listaBicicletas;
+		} catch (SQLException e) {
+			System.out.println(e);
 		}
 	}
+
 	
 }
