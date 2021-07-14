89ee1abc1fe603c3a998a7d5fdd762c29c3ce24f
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-07-14 08:54:50 -0300

    método para obter lista de bicicletas adicionado

--- src/br/com/locacao/bean/BicicletaBean.java
+++ src/br/com/locacao/bean/BicicletaBean.java
@@ -1,13 +1,11 @@
 package br.com.locacao.bean;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
 import br.com.locacao.dao.BicicletaDAO;
 import br.com.locacao.modelo.Bicicleta;
 
@@ -15,7 +13,14 @@ import br.com.locacao.modelo.Bicicleta;
 @ViewScoped
 public class BicicletaBean {
 	private Bicicleta bicicleta = new Bicicleta();
-	private List<Bicicleta> bicicletas = new ArrayList<Bicicleta>();
+	private BicicletaDAO daoBicicleta = new BicicletaDAO();
 	
+	public Bicicleta getBicicleta() {
+		return bicicleta;
+	}
+	
+	public List<Bicicleta> getBicicletas() throws SQLException{
+		return this.daoBicicleta.listaBicicletas();
+	}
 	
 }
