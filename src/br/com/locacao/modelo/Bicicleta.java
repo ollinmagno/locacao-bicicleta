ed6ab1a0a469c6e3c5e9d215c952d7666b4d7eac
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-07-15 13:39:15 -0300

    modificacao no método toString

--- src/br/com/locacao/modelo/Bicicleta.java
+++ src/br/com/locacao/modelo/Bicicleta.java
@@ -7,57 +7,70 @@ public class Bicicleta {
 	private int marchas;
 	private int aro;
 	private Modelo modelo;
-	
+
 	public Bicicleta() {
-		
+
 	}
+
 	public Bicicleta(Integer id) {
 		this.id = id;
 	}
+
 	public Bicicleta(Modelo modelo) {
 		this.modelo = modelo;
 	}
+
 	public Bicicleta(int aro, String cor, int marchas, Modelo modelo) {
 		this.cor = cor;
 		this.marchas = marchas;
 		this.aro = aro;
 		this.modelo = modelo;
 	}
-	
+
 	public Modelo getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(Modelo modelo) {
 		this.modelo = modelo;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getCor() {
 		return cor;
 	}
+
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
+
 	public int getMarchas() {
 		return marchas;
 	}
+
 	public void setMarchas(int marchas) {
 		this.marchas = marchas;
 	}
+
 	public int getAro() {
 		return aro;
 	}
+
 	public void setAro(int aro) {
 		this.aro = aro;
 	}
+
 	@Override
 	public String toString() {
-		return "Bicicleta [id=" + id + ", cor=" + cor + ", marchas=" + marchas + ", aro=" + aro + ", modelo=" + modelo.getMarca()
-				+ "]";
+		return "Bicicleta [id=" + id + ", cor=" + cor + ", marchas=" + marchas + ", aro=" + aro + ", modelo="
+				+ modelo.getMarca() + ", Preço por hora=" + modelo.getPrecoPorHora() + "]";
 	}
-	
+
 }
