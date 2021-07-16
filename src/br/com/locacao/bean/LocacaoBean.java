ece1f3dfd21b9c22e2a7dfb14d7e2bc338118e88
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-07-15 22:35:49 -0300

    método para buscar locacao

--- src/br/com/locacao/bean/LocacaoBean.java
+++ src/br/com/locacao/bean/LocacaoBean.java
@@ -1,9 +1,22 @@
 package br.com.locacao.bean;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import br.com.locacao.dao.LocacaoDAO;
+import br.com.locacao.modelo.Locacao;
 
 @ManagedBean
 public class LocacaoBean {
+	private Locacao locacao = new Locacao();
+	private LocacaoDAO daoLocacao = new LocacaoDAO();
 	
+	public Locacao getLocacao() {
+		return this.locacao;
+	}
+	public List<Locacao> getLocacoes() throws SQLException{
+		return this.daoLocacao.historicoGeralDeLocacao();
+	}
 	
 }
