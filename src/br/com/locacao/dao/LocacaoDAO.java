5f48515e052cf72ac23246d5558d36645e513789
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-07-17 12:03:38 -0300

    método de histórico modificado

--- src/br/com/locacao/dao/LocacaoDAO.java
+++ src/br/com/locacao/dao/LocacaoDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 import br.com.locacao.modelo.Bicicleta;
 import br.com.locacao.modelo.Cliente;
 import br.com.locacao.modelo.Locacao;
+import br.com.locacao.modelo.Modelo;
 import br.com.locacao.modelo.Situacao;
 
 public class LocacaoDAO {
@@ -35,19 +36,29 @@ public class LocacaoDAO {
 	public List<Locacao> historicoGeralDeLocacao() throws SQLException {
 		List<Locacao> historicoDeLocacoes = new ArrayList<Locacao>();
 
-		String sql = "SELECT HORA_DA_LOCACAO, DEVOLUCAO, NOME, CPF, STATUS\r\n"
-				+ "FROM LOCACAO JOIN CLIENTE ON ID_CLIENTE = CLIENTE.ID JOIN SITUACAO ON ID_SITUACAO = SITUACAO.ID";
+		String sql = "SELECT HORA_DA_LOCACAO, DEVOLUCAO, NOME, CPF, STATUS, BICICLETA.ID,\r\n" + 
+				"BICICLETA.ARO, BICICLETA.COR, BICICLETA.MARCHAS,\r\n" + 
+				"MODELO.MARCA, MODELO.PRECO_POR_HORA\r\n" + 
+				"FROM LOCACAO JOIN CLIENTE ON ID_CLIENTE = CLIENTE.ID JOIN SITUACAO ON\r\n" + 
+				"ID_SITUACAO = SITUACAO.ID JOIN BICICLETA ON ID_BICICLETA = BICICLETA.ID\r\n" + 
+				"JOIN MODELO ON ID_BICICLETA = MODELO.ID";
 
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
 				try (ResultSet resultset = pstm.executeQuery()) {
 					while (resultset.next()) {
-						Locacao locacao = new Locacao(new Cliente(), new Bicicleta(), new Situacao());
+						Locacao locacao = new Locacao(new Cliente(), new Bicicleta(new Modelo()), new Situacao());
 						// locacao.setHoraDaLocacao(resultset.getInt(1));
 						// locacao.setDevolucacao(resultset.getInt(2));
 						locacao.getCliente().setNome(resultset.getString(3));
 						locacao.getCliente().setCpf(resultset.getString(4));
 						locacao.getSituacao().setStatus(resultset.getString(5));
+						locacao.getBicicleta().setId(resultset.getInt(6));
+						locacao.getBicicleta().setAro(resultset.getInt(7));
+						locacao.getBicicleta().setCor(resultset.getString(8));
+						locacao.getBicicleta().setMarchas(resultset.getInt(9));
+						locacao.getBicicleta().getModelo().setMarca(resultset.getString(10));
+						locacao.getBicicleta().getModelo().setPrecoPorHora(resultset.getDouble(11));
 						historicoDeLocacoes.add(locacao);
 					}
 				} catch (SQLException e) {
