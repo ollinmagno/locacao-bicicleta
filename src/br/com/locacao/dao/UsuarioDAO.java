b8d5dfee2f09dfa8d5a431a14121f260193f2e3d
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-08-02 21:26:59 -0300

    método de autenticar usuário

--- src/br/com/locacao/dao/UsuarioDAO.java
+++ src/br/com/locacao/dao/UsuarioDAO.java
@@ -4,14 +4,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import br.com.locacao.modelo.Bicicleta;
 import br.com.locacao.modelo.Cliente;
 
 public class UsuarioDAO {
-	
-	public boolean existe(Cliente cliente) throws SQLException {
-		return true;
+
+	public boolean usuarioExiste(Cliente cliente) throws SQLException {
+		String sql = "SELECT EMAIL, SENHA FROM CLIENTE";
+		boolean autenticaUsuario = false;
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				try (ResultSet result = pstm.executeQuery()) {
+					while (result.next()) {
+						boolean emailValido = cliente.getEmail().equals(result.getString(1));
+						boolean senhaValida = cliente.getSenha().equals(result.getString(2));
+						if(emailValido && senhaValida) {
+							autenticaUsuario = true;
+						}
+					}
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return autenticaUsuario;
 	}
 }
