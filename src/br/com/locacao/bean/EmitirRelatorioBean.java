1380663361a5b58ed50819bf0b46b2b204d5ca8f
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2021-11-19 13:54:02 -0300

    correção no método de gerar relatório

--- src/br/com/locacao/bean/EmitirRelatorioBean.java
+++ src/br/com/locacao/bean/EmitirRelatorioBean.java
@@ -2,19 +2,20 @@ package br.com.locacao.bean;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.servlet.ServletException;
 
 import br.com.locacao.relatorio.GerarRelatorio;
+import net.sf.jasperreports.engine.JRException;
 
 @ManagedBean
 @ViewScoped
 public class EmitirRelatorioBean {
-	
-	public void emitirRelatorioAction() throws SQLException, ServletException, IOException {
+	public void emitirRelatorioAction() throws SQLException, ServletException, IOException, JRException {
 		GerarRelatorio gerarRelatorio = new GerarRelatorio();
-		gerarRelatorio.gerarRelatorio();
+		gerarRelatorio.gerarRelatorio("lista-bike", new HashMap<String, Object>(), "lista-bicicleta");
 	}
 }
--- src/br/com/locacao/relatorio/GerarRelatorio.java
+++ src/br/com/locacao/relatorio/GerarRelatorio.java
@@ -10,46 +10,47 @@ import java.util.Map;
 
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.locacao.dao.ConnectionFactory;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperRunManager;
 
 public class GerarRelatorio {
 	private HttpServletResponse response;
 	private FacesContext context;
-	private ByteArrayOutputStream baos;
-	private InputStream stream;
-	
+
 	public GerarRelatorio() {
 		this.context = FacesContext.getCurrentInstance();
 		this.response = (HttpServletResponse) context.getExternalContext().getResponse();
 	}
 
-	public void gerarRelatorio() throws SQLException, ServletException, IOException {
+	public void gerarRelatorio(String nomeRelatorio, Map<String, Object> parametros, String nomeArquivo) throws SQLException, ServletException, IOException, JRException {
+		String relatorio = "/WEB-INF/jasper/"+nomeRelatorio+".jasper";
+
+		InputStream reportStream = context.getExternalContext().getResourceAsStream(relatorio);
+
+		response.setContentType("application/pdf");
+		response.setHeader("Content-disposition", "attachment;filename="+nomeArquivo+".pdf");
+
+		ServletOutputStream servletOutputStream = response.getOutputStream();
+
 		Connection connection = null;
-		stream = Thread.currentThread().getClass().getResourceAsStream("/WEB-INF/jasper/listabike.jasper");
-		Map<String, Object> parametros = new HashMap<String, Object>();
-		baos = new ByteArrayOutputStream();
-		
+
 		try {
 			connection = new ConnectionFactory().recuperarConexao();
-			
-			Relatorio gerador = new Relatorio(this.stream, parametros, connection, this.baos);
-			gerador.geraPDF();
-			response.reset();
-			response.setContentType("application/pdf");
-			response.setContentLength(baos.size());
-			response.setHeader("Content-disposition", "inline; filename=relatorio.pdf");
-			response.getOutputStream().write(baos.toByteArray());
-			response.getOutputStream().flush();
-			response.getOutputStream().close();
-			context.responseComplete();
+
+			JasperRunManager.runReportToPdfStream(reportStream, response.getOutputStream(), parametros, connection);
 
 		} catch (SQLException e) {
 			throw new ServletException(e);
 		} finally {
 			connection.close();
 		}
+
+		FacesContext.getCurrentInstance().responseComplete();
+		servletOutputStream.flush();
+		servletOutputStream.close();
 	}
 }
