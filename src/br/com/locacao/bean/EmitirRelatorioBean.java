0a11e327ed83776ce95e1a943d2ed8c5c55c612f
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2021-11-22 13:35:48 -0300

    nome do arquivo no parametro alterado

--- src/br/com/locacao/bean/EmitirRelatorioBean.java
+++ src/br/com/locacao/bean/EmitirRelatorioBean.java
@@ -2,8 +2,11 @@ package br.com.locacao.bean;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.servlet.ServletException;
@@ -14,8 +17,9 @@ import net.sf.jasperreports.engine.JRException;
 @ManagedBean
 @ViewScoped
 public class EmitirRelatorioBean {
+	
 	public void emitirRelatorioAction() throws SQLException, ServletException, IOException, JRException {
 		GerarRelatorio gerarRelatorio = new GerarRelatorio();
-		gerarRelatorio.gerarRelatorio("lista-bike", new HashMap<String, Object>(), "lista-bicicleta");
+		gerarRelatorio.gerarRelatorio("lista-bike-cor", new HashMap<String, Object>(), "relatorio");
 	}
 }
